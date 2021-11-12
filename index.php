1f14651f17f8ea0568e91c12cdf5e1cb30a8c0e0
datmvph17185@fpt.edu.vn <datmvph17185@fpt.edu.vn>
2021-11-12 16:57:54 +0700

    dat

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
 <?php
-
-
-echo "Hiệp óc chó ngu nhât Việt Nam   ấccasc";
-echo "âcsca";
-echo "âcsca";
-echo 'hIEEU OC CHO  !!!';
-echo "Mới tạo";
-
-echo "Hiếu đi nặng!!!";
-
-echo "Hiếu đi ỉa!!!";
-
-echo "Dat 09";
-$ocChoHiep = ["nu", "acc", "âcsc"];
-echo "</br><pre>";
-var_dump($ocChoHiep);
