9d7667c5cf3c217ee972fde7b66b9a0eba25951a
datmvph17185@fpt.edu.vn <datmvph17185@fpt.edu.vn>
2021-12-02 16:04:25 +0700

    dat

---
+++ site/cart/cart.php
@@ -0,0 +1,118 @@
+<div class="uk-margin-large-top uk-container uk-text-center">
+    <!-- <img class="page-cart__img" src="../content/img/pages/cart/img-cart.png" alt="">
+                    <div class="page-cart__box">
+                        <div class="page-cart__title">Giỏ hàng trống.</div><a class="uk-button" href="page-catalog.html">Đi mua
+                            hàng</a>
+                    </div> -->
+    <div class="uk-text-center" uk-grid>
+        <div class="uk-overflow-auto uk-width-expand cart_table">
+            <table class="uk-table uk-table-small uk-table-divider">
+                <thead>
+                    <tr>
+                        <th>Tên món</th>
+                        <th>Kích cỡ</th>
+                        <th>Topping</th>
+                        <th>Giá</th>
+                        <th>Giảm giá</th>
+                        <th>Số lượng</th>
+                        <th>Tổng cộng</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="uk-text-left">
+                    <tr>
+                        <td>
+                            <!-- <div class="uk-align-right">
+                                <img src="https://images.unsplash.com/photo-1522201949034-507737bce479?fit=crop&w=650&h=433&q=80" width="100" height="" alt="" uk-img>
+                            </div> -->
+                            <span>Pizza thơm ngon</span>
+                        </td>
+                        <td>100.000đ</td>
+                        <td>100.000đ</td>
+                        <td>100.000đ</td>
+                        <td>100.000đ</td>
+                        <td>
+                            <div class="row">
+                                <div class="col">
+                                    <span class="counter">
+                                        <span class="minus">-</span><input type="text" value="1" />
+                                        <span class="plus">+</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </td>
+                        <td>100.000đ</td>
+                        <td><button uk-icon="trash"></button></td>
+                    </tr>
+                    <!-- <tr>
+                        <td>
+                            <div class="uk-align-right">
+                                <img src="https://images.unsplash.com/photo-1522201949034-507737bce479?fit=crop&w=650&h=433&q=80" width="100" height="" alt="" uk-img>
+                            </div>
+                            <span>Pizza thơm ngon</span>
+                        </td>
+                        <td>100.000đ</td>
+                        <td>
+                            <div class="row">
+                                <div class="col">
+                                    <span class="counter">
+                                        <span class="minus">-</span><input type="text" value="1" />
+                                        <span class="plus">+</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </td>
+                        <td>100.000đ</td>
+                        <td><button uk-icon="trash"></button></td>
+
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="uk-align-right">
+                                <img src="https://images.unsplash.com/photo-1522201949034-507737bce479?fit=crop&w=650&h=433&q=80" width="100" height="" alt="" uk-img>
+                            </div>
+                            <span>Pizza DOUBLE CHEESE BURGER (L)DOUBLE CHEESE BURGER (L)</span>
+                        </td>
+                        <td>100.000đ</td>
+                        <td>
+                            <div class="row">
+                                <div class="col">
+                                    <span class="counter">
+                                        <span class="minus">-</span><input type="text" value="1" />
+                                        <span class="plus">+</span>
+                                    </span>
+                                </div>
+                            </div>
+                        </td>
+                        <td>100.000đ</td>
+                        <td><button uk-icon="trash"></button></td>
+
+                    </tr> -->
+                </tbody>
+            </table>
+        </div>
+        <div class="uk-width-1-4@l cart_checkout">
+            <div class="uk-card uk-padding-small uk-card-background">
+                <h3 class="uk-card-title">Giỏ hàng</h3>
+                <div uk-grid>
+                    <div class="uk-width-1-2">Thành tiền:</div>
+                    <div class="uk-width-1-2">470.000 ₫</div>
+                </div>
+                <div uk-grid>
+                    <div class="uk-width-1-2">Thuế:</div>
+                    <div class="uk-width-1-2">32.900 ₫</div>
+                </div>
+                <hr class="uk-divider-icon">
+                <div uk-grid>
+                    <div class="uk-width-1-2">TỔNG CỘNG:</div>
+                    <div class="uk-width-1-2 uk-text-danger uk-text-lead uk-text-bolder">502.900 ₫</div>
+                </div>
+                <div uk-margin>
+                    <button class="uk-button uk-button-danger">Thanh toán</button>
+                    <button class="uk-button uk-button-default">Tiếp tục đặt món</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
--- site/cart/index.php
+++ site/cart/index.php
@@ -6,14 +6,14 @@ require '../../dao/user.php';
 extract($_REQUEST);
 
 if (exist_param("form_invoice")) {
-    if (isset($_SESSION['user'])) {
-        $id = $_SESSION['user'];
-        extract($kh);
-        $VIEW_NAME = "../cart/form_invoice.php";
-    }else{
-        header("location:".SITE_URL."tai_khoan/dang_nhap.php");
-    }
+    // if (isset($_SESSION['user'])) {
+    //     $id = $_SESSION['user'];
+    //     extract($kh);
+    //     $VIEW_NAME = "../cart/form_invoice.php";
+    // }else{
+    //     header("location:".SITE_URL."tai_khoan/dang_nhap.php");
+    // }
 } else {
     $VIEW_NAME = "../cart/cart.php";
 }
-require '../template.php';
+require '../layout.php';
